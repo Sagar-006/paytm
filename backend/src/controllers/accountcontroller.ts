977c491fc2b9c56058355a35580ef3b51c2d772f
Sagar-006 <sagarbiradar7030@gmail.com>
2025-08-12 13:27:12 +0530

    Backend completed-

--- backend/src/controllers/accountcontroller.ts
+++ backend/src/controllers/accountcontroller.ts
@@ -1,21 +1,63 @@
-import { type Response,type Request } from "express"
-import { UserBalance } from "../db.js";
-export const getBalance = async(req:Request,res:Response) => {
-    // @ts-ignore
-    const userId = req.userId;
-    console.log(userId,"on getBalance route")
+import { type Response, type Request } from "express";
+import { User, UserBalance } from "../db.js";
+import mongoose from "mongoose";
+export const getBalance = async (req: Request, res: Response) => {
+  // @ts-ignore
+  const userId = req.userId;
+  console.log(userId, "on getBalance route");
 
-    try{
-        const response = await UserBalance.findOne({
-            userId:userId
-        });
-        console.log(response)
-        res.status(200).json({
-            balance:response?.balance
+  try {
+    const response = await UserBalance.findOne({
+      userId: userId,
+    });
+    console.log(response);
+    res.status(200).json({
+      balance: response?.balance,
+    });
+  } catch (e) {
+    res.status(500).send(e);
+  }
+};
+
+export const transferMoney = async (req: Request, res: Response) => {
+  const session = await mongoose.startSession();
+  // @ts-ignore
+  const userId = req.userId;
+  // const to = req.params.to;
+  
+    session.startTransaction();
+
+    const { amount, to } = req.body;
+
+    const account = await UserBalance.findOne({
+      userId: userId,
+    });
+
+    const currentBalance = account?.balance;
+    if (!currentBalance) return;
+
+    if (!account || currentBalance < amount) {
+        await session.abortTransaction();
+      return res.status(400).json({
+        message: "Insufficient Balance!",
+      });
+    }
+
+    const toAccount = await UserBalance.findOne({userId:to});
+
+    if(!toAccount){
+        await session.abortTransaction();
+        return res.status(400).json({
+            message:"Invalid account"
         })
     }
-    catch(e){
-        res.status(500).send(e);
-    }
 
-}
+    await UserBalance.updateOne({userId:userId},{$inc:{balance: - amount}}).session(session);
+    await UserBalance.updateOne({userId:to},{$inc:{balance: + amount}}).session(session);
+
+    await session.commitTransaction();
+    res.status(200).json({
+      message: "balance added!",
+    });
+  
+};
--- backend/src/routes/accounts.ts
+++ backend/src/routes/accounts.ts
@@ -1,9 +1,10 @@
 import express from 'express'
 import { authMiddleware } from '../middlewares/authMiddleware.js';
-import { getBalance } from '../controllers/accountcontroller.js';
+import { getBalance, transferMoney } from '../controllers/accountcontroller.js';
 
 const router = express.Router();
 
 router.get("/balance",authMiddleware,getBalance);
+router.post("/transfer",authMiddleware,transferMoney)
 
-export default router
+export default router;
